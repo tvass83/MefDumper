6f4eaf13da84ef3ff930e7715b1fa9c5ee4448e1
tvass83 <tamas.vass83@gmail.com>
2020-05-25 22:19:36 +0200

    Add a simple cmd-line argument parser

---
+++ MefDumper/ArgParser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MefDumper
+{
+    public enum ErrorCode
+    {
+        Success = 0,
+        NoArgument,
+        WhiteSpace,
+        MissingValueForSwitch,
+        InvalidValueForSwitch,
+    }
+
+    public static class ArgParser
+    {
+        public static List<string> Switches { get; set; } = new List<string>();
+        public static Dictionary<string, string> SwitchesWithValues { get; set; } = new Dictionary<string, string>();
+        public static List<string> OrdinaryArguments { get; set; } = new List<string>();
+
+        public static ErrorCode Parse(string[] args, string[] expectedSwitches)
+        {
+            if (args.Length == 0)
+            {
+                return ErrorCode.NoArgument;
+            }
+
+            string lastSwitch = null;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i].ToLowerInvariant();
+
+                if (string.IsNullOrWhiteSpace(arg))
+                {
+                    return ErrorCode.WhiteSpace;
+                }
+
+                if (arg.StartsWith("-", StringComparison.InvariantCulture))
+                {
+                    if (lastSwitch != null)
+                    {
+                        return ErrorCode.InvalidValueForSwitch;
+                    }
+
+                    if (expectedSwitches.Contains(arg))
+                    {
+                        Switches.Add(arg);
+                    }
+                    else
+                    {
+                        lastSwitch = arg;
+                    }
+                }
+                else if (lastSwitch != null)
+                {
+                    SwitchesWithValues[lastSwitch] = arg;
+                    lastSwitch = null;
+                }
+                else
+                {
+                    OrdinaryArguments.Add(arg);
+                }
+            }
+
+            if (lastSwitch != null)
+            {
+                return ErrorCode.MissingValueForSwitch;
+            }
+
+            return ErrorCode.Success;
+        }
+    }
+}
